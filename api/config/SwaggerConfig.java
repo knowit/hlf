7437b1afe33b684a64a572a7f736c5d7f682ab29
Lisamso <lisasoem@live.no>
2018-07-04 09:43:43 +0200

    Made example SwaggerConfig file.

--- api/config/SwaggerConfig.java
+++ api/config/SwaggerConfig.java
@@ -1,11 +1,24 @@
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "Name", "URL", "e-mail");
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "API Title", "API Description", "1.0",
+            "urn:tos", DEFAULT_CONTACT,
+            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<String>(Arrays.asList("application/json",
+                    "application/xml"));
+
     @Bean
-    public Docket productApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()                 .apis(RequestHandlerSelectors.basePackage("godlyd.no.hlf.godlyd.api.controller"))
-                .paths(regex("/*"))
-                .build();
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
