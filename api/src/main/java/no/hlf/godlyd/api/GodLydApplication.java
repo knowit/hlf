fc85bfeddbd82d206964efbf85007f64026925c0
Maria Jensen <maria.jensen@knowit.no>
2018-07-05 13:07:33 +0200

    Auth0

--- api/src/main/java/no/hlf/godlyd/api/GodLydApplication.java
+++ api/src/main/java/no/hlf/godlyd/api/GodLydApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -31,12 +32,10 @@ public class GodLydApplication {
 	    run(GodLydApplication.class, args);
 	}
 
-	/*
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
 	    return new BCryptPasswordEncoder();
     }
-    */
 
     /*
 	@Bean
---
+++ api/src/main/java/no/hlf/godlyd/api/config/security/JWTAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package no.hlf.godlyd.api.config.security;
+
+import no.hlf.godlyd.api.model.Bruker;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static no.hlf.godlyd.api.config.security.SecurityConstants.*;
+
+// Filter for authenticating users
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+    }
+
+    // Where we parse the user's credentials and issue them to the AuthenticationManager.
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException{
+        try{
+            Bruker creds = new ObjectMapper().readValue(req.getInputStream(), Bruker.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getBrukernavn(),
+                            creds.getPassord(),
+                            new ArrayList<>())
+            );
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    // The method called when a user successfully logs in. We use this method to generate a JWT for this user.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException{
+        String token = Jwts.builder()
+                .setSubject(((User) auth.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
+                .compact();
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+
+    }
+
+
+}
