7f3453b983fbe47b4515e5320e78f1326f407363
Lisamso <lisasoem@live.no>
2018-07-17 12:23:45 +0200

    Updated Bruker model.

--- api/src/main/java/no/hlf/godlyd/api/model/Bruker.java
+++ api/src/main/java/no/hlf/godlyd/api/model/Bruker.java
@@ -19,46 +19,18 @@ public class Bruker implements Serializable {
     private Integer id;
 
     @Column(unique = true)
-    private String brukernavn;
+    private String auth0UserId;
 
-    private String passord;
     private String fornavn;
     private String etternavn;
 
-    @Column(unique = true)
-    private String epost;
-
     @OneToMany(mappedBy = "registrator")
     private List<Vurdering> vurderinger;
-
-    public Bruker(String brukernavn, String epost) {
-        this.brukernavn = brukernavn;
-        this.epost = epost;
-    }
-
-    public Bruker(String brukernavn, String passord, String fornavn, String etternavn, String epost){
-        this.brukernavn = brukernavn;
-        this.passord = passord;
-        this.fornavn = fornavn;
-        this.etternavn = etternavn;
-        this.epost = epost;
-    }
-
-    public Bruker(){}
-
 
     // Getters og setters
     public Integer getId() { return id; }
 
     public void setId(Integer id) { this.id = id; }
-
-    public String getBrukernavn() { return brukernavn; }
-
-    public void setBrukernavn(String brukernavn) { this.brukernavn = brukernavn; }
-
-    public String getPassord() { return passord; }
-
-    public void setPassord(String passord) { this.passord = passord; }
 
     public String getFornavn() { return fornavn; }
 
@@ -67,10 +39,6 @@ public class Bruker implements Serializable {
     public String getEtternavn() { return etternavn; }
 
     public void setEtternavn(String etternavn) { this.etternavn = etternavn; }
-
-    public String getEpost() { return epost; }
-
-    public void setEpost(String epost) { this.epost = epost; }
 
     @JsonIgnore
     public List<Vurdering> getVurderinger() { return vurderinger; }
