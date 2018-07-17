37cf18462624ce0f43fdc96f8f2dabcfc61b75fd
Lisamso <lisasoem@live.no>
2018-07-17 16:05:07 +0200

    Added functionality to refresh invalid tokens.

--- app/containers/LoginScreen.js
+++ app/containers/LoginScreen.js
@@ -18,38 +18,57 @@ export default class LoginScreen extends Component{
 
     constructor(props){
         super(props);
-        this.creds = '';
+        //AsyncStorage.clear();
+        this.authenticate = true;
+        this._getLocalToken();
+        
     }
 
     render(){
-        return <View>
+        return this.authenticate ? ( <View>
             <AppButton onPress={() => this.login()}>
             Logg inn
         </AppButton>
         <AppButton onPress={() => this.printState()}>
             Print state
         </AppButton>
-        </View>
+        </View>):
+        (<View>
+            <AppButton onPress={() => this.printState()}>
+            Print state
+            </AppButton>
+            </View>)
 
     }
 
     printState(){
+        console.log(this.access_token);
+    }
+
+    _getLocalToken = async () => {
         AsyncStorage.getItem("access_token")
         .then(value =>
-            console.log("access_token: "+value)
-        ).done();
-        AsyncStorage.getItem("id_token")
-        .then(value =>
-            console.log("id_token: "+value)
-        ).done();
-        AsyncStorage.getItem("refresh_token")
-        .then(value =>
-            console.log("refresh_token: "+value)
+            this.checkToken(value)
         ).done();
     }
 
     login(){
         this._authorize();
+    }
+
+    checkToken(token){
+        this.access_token = token;
+        this.checkAuthorization();
+        if(this.authenticate){
+            this.renewToken();
+        }
+    }
+
+    renewToken(){
+        AsyncStorage.getItem("refresh_token")
+        .then(value =>
+            this._refreshToken(value)
+        ).done();
     }
 
     _authorize = async () => {        
@@ -63,10 +82,39 @@ export default class LoginScreen extends Component{
 
     }
 
+    _refreshToken = async(rToken) =>{
+        if(rToken != null && rToken != ''){
+            auth0.auth.refreshToken({refreshToken: rToken, scope: config.scope})
+        .then(credentials =>
+            this.saveCredentials(credentials)
+        )
+        }
+        else{
+            this.forceUpdate();
+        }
+    }
+
+    checkAuthorization(){
+        this.authenticate = !this.isAuthenticated(this.access_token);
+        this.forceUpdate();
+    }
+
     saveCredentials(credentials){
+        this.access_token = credentials.accessToken;
+        this.checkAuthorization();
         AsyncStorage.setItem("access_token", credentials.accessToken)
         AsyncStorage.setItem("id_token", credentials.idToken)
         AsyncStorage.setItem("refresh_token", credentials.refreshToken)
+    }
+
+    isAuthenticated(token){
+        if(token == '' || token == null){
+            return false;
+        }
+        else{
+            // Check with api that token is authenticated
+            return true;
+        }
     }
 
 }
