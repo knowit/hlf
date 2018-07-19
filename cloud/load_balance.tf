0fe648ca1c48f6bab78a896551a80071d487390d
rune <myklevoll@gmail.com>
2018-07-19 12:02:11 +0200

    moved load balancer resources

--- cloud/load_balance.tf
+++ cloud/load_balance.tf
@@ -1,3 +1,24 @@
+resource "google_compute_backend_service" "load-balancer" {
+  name        = "loadbalancer"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+  enable_cdn  = false
+
+  backend = {
+    group = "${google_compute_instance_group.server_group.self_link}"
+  }
+
+  health_checks = ["${google_compute_http_health_check.default_health_check.self_link}"]
+}
+
+resource "google_compute_http_health_check" "default_health_check" {
+  name               = "default-healthcheck"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+
 resource "google_compute_url_map" "god_lyd_map" {
   name            = "godlyd-map"
   default_service = "${google_compute_backend_service.load-balancer.self_link}"
