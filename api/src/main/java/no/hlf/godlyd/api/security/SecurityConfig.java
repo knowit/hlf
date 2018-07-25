cde9934820c83260ebc1531f60b349bde95d3861
Lisamso <lisasoem@live.no>
2018-07-25 13:18:10 +0200

    Fixed authentication for http request methods other than GET.

--- api/src/main/java/no/hlf/godlyd/api/security/SecurityConfig.java
+++ api/src/main/java/no/hlf/godlyd/api/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .configure(http)
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/healthcheck").permitAll()
-                .antMatchers(HttpMethod.GET, "/**").authenticated()
+                .antMatchers("/**").authenticated()
                 .and()
                 .logout().permitAll();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
